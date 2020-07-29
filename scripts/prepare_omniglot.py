01e6e6c4a7e6b978629eb29fce7011393b486a9c
Valerio Biscione <valerio.biscione@gmail.com>
2020-07-29 11:01:52 +0200

    Prepare omniglot no rotation

--- scripts/prepare_omniglot.py
+++ scripts/prepare_omniglot.py
@@ -9,6 +9,7 @@ Networks papers.
 2. Downsize images to 28x28
 3. Uses background and evaluation sets present in the raw dataset
 """
+import numpy as np
 from skimage import io
 from skimage import transform
 import zipfile
@@ -24,38 +25,51 @@ dataset_zip_files = ['images_background.zip', 'images_evaluation.zip']
 raw_omniglot_location = DATA_PATH + '/Omniglot_Raw/'
 prepared_omniglot_location = DATA_PATH + '/Omniglot/'
 output_shape = (28, 28)
+do_rotation = False
 
-
-def handle_characters(alphabet_folder, character_folder, rotate):
+def handle_characters(alphabet_folder, character_folder, rotate=None):
     for root, _, character_images in os.walk(character_folder):
         character_name = root.split('/')[-1]
-        mkdir(f'{alphabet_folder}.{rotate}/{character_name}')
+        #mkdir(f'{alphabet_folder}.{rotate}/{character_name}')
         for img_path in character_images:
             # print(root+'/'+img_path)
             img = io.imread(root+'/'+img_path)
-            img = transform.rotate(img, angle=rotate)
+            if rotate is not None:
+                img = transform.rotate(img, angle=rotate)
             img = transform.resize(img, output_shape, anti_aliasing=True)
             img = (img - img.min()) / (img.max() - img.min())
             # print(img.min(), img.max())
             # print(f'{alphabet_folder}.{rotate}/{character_name}/{img_path}')
-            io.imsave(f'{alphabet_folder}.{rotate}/{character_name}/{img_path}', img)
+            if rotate is not None:
+                io.imsave(f'{alphabet_folder}.{rotate}/{character_name}/{img_path}', img)
+            else:
+                io.imsave(f'{alphabet_folder}/{character_name}/{img_path}', img)
             # return
 
 
 def handle_alphabet(folder):
     print('{}...'.format(folder.split('/')[-1]))
-    for rotate in [0, 90, 180, 270]:
-        # Create new folders for each augmented alphabet
-        mkdir(f'{folder}.{rotate}')
+    if do_rotation:
+        for rotate in [0, 90, 180, 270]:
+            # Create new folders for each augmented alphabet
+            mkdir(f'{folder}.{rotate}')
+            for root, character_folders, _ in os.walk(folder):
+                for character_folder in character_folders:
+                    # For each character folder in an alphabet rotate and resize all of the images and save
+                    # to the new folder
+                    handle_characters(folder, root + '/' + character_folder, rotate)
+                    # return
+    else:
+        mkdir(f'{folder}')
         for root, character_folders, _ in os.walk(folder):
             for character_folder in character_folders:
                 # For each character folder in an alphabet rotate and resize all of the images and save
                 # to the new folder
-                handle_characters(folder, root + '/' + character_folder, rotate)
-                # return
+                handle_characters(folder, root + '/' + character_folder, rotate=None)
+
 
     # Delete original alphabet
-    rmdir(folder)
+    #rmdir(folder)
 
 
 # Clean up previous extraction
